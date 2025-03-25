889493dc5f43dc3d9948987344a071d5bfd16bce
Arlena-code <arlena88@qq.com>
2025-03-25 21:27:20 +0800

    库名一致

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/EETLWeb/',
+  base: '/EETL-Web/',
   plugins: [react()],
   publicDir: 'public', // 确保指向正确的公共目录
   resolve: {
